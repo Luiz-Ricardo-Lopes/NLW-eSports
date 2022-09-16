7a8af0efc9de5ea604d2e7690a949f0a40963f71
Luiz Lopes <luizrl@yahoo.com.br>
2022-09-16 19:27:50 -0300

    construção do back-end da aplicação

---
+++ server/src/utils/convert-hour-string-to-minutes.ts
@@ -0,0 +1,9 @@
+// 15:00 -> ["15", "00"] -> [18, 00]
+
+export function convertHourStringToMinutes(hourString: string){
+  const [hours, minutes] = hourString.split(':').map(Number)
+
+  const minutesAmount = (hours * 60 ) + minutes;
+
+  return minutesAmount
+}
